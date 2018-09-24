27cf2572999ec42ca0aa847a92ef449de5c8c8ed
Ashish <Ashish@10.0.0.243>
2018-09-24 00:01:36 -0400

    Test InRhythm 2

--- InRhythm/src/com/inRhythm/TestingChallenge.java
+++ InRhythm/src/com/inRhythm/TestingChallenge.java
@@ -7,7 +7,7 @@ public class TestingChallenge {
 	public static void main(String[] args) {
 		Sentence sen = new Sentence();
 		SentenceObject senObj = new SentenceObject();
-		senObj = sen.wordLength("The cow jumped over the Ashish");
+		senObj = sen.wordLength("The cow jumped over the moon");
 		System.out.println("Longest word: " + senObj.getWord() + "\nWord Length : " + senObj.getWordLength() + " ");
 	}
 
