0b9adb1f1c6cae622b09ac7592554b00b1e28265
Ashish <Ashish@10.0.0.243>
2018-09-23 23:55:49 -0400

    Test InRhythm 1

---
+++ InRhythm/src/com/inRhythm/pojo/SentenceObject.java
@@ -0,0 +1,20 @@
+package com.inRhythm.pojo;
+
+public class SentenceObject {
+
+	private String word;
+	private int wordLength;
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public int getWordLength() {
+		return wordLength;
+	}
+	public void setWordLength(int wordLength) {
+		this.wordLength = wordLength;
+	}
+	
+}
